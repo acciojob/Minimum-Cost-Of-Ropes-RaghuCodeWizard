6177e25eabc0910f932dd2c1632e1ff51de6b6c6
Raghavendra M <137168147+RaghuCodeWizard@users.noreply.github.com>
2023-09-20 11:49:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-function calculateMinCost() {
-  //your code here
-  
-  
-  
-}  
+function calculateMinCost(ropeLengths) {
+  const minHeap = [...ropeLengths];
+
+  function connectRopes(ropeLengths) {
+    let cost = 0;
+
+    while (ropeLengths.length > 1) {
+      const min1 = Math.min(...ropeLengths);
+      const index1 = ropeLengths.indexOf(min1);
+      ropeLengths.splice(index1, 1);
+
+      const min2 = Math.min(...ropeLengths);
+      const index2 = ropeLengths.indexOf(min2);
+      ropeLengths.splice(index2, 1);
+
+      const connectedRope = min1 + min2;
+      cost += connectedRope;
+
+      ropeLengths.push(connectedRope);
+    }
+
+    return cost;
+  }
+
+  return connectRopes(minHeap);
+}
